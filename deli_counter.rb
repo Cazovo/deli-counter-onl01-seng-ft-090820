ed52047c786fb758ef3125c3cb619c9a561f727d
conner fortner <fortner.conner@yahoo.com>
2020-09-17 00:22:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,36 +1,4 @@
-def line(deli)
-counter = 1
-    output = "The line is currently:"
-    if deli.length == 0 
-        puts "The line is currently empty."
-    else
-        deli.each do |person| 
-            output += " #{counter}. " + person  
-            counter += 1 
-        end
-        puts output
-    end
-end		
 
-
-def take_a_number(line, new_customer)
-    line.push(new_customer)
-    position = line.index(new_customer)
-    puts "Welcome, #{new_customer}. You are number #{line.length} in line"
-    return name, position
-    end
-end
-  
-  
-
-def now_serving(line)
-    if line.length == 0
-       puts "There is nobody waiting to be served!"
-    else 
-       puts "Currently serving #{line[0]}." 
-       line.shift
-    end
-end
 
 
     
