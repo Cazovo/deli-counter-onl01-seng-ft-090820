539905042f5907a466c2b8b44da7fae62238db53
conner fortner <fortner.conner@yahoo.com>
2020-09-17 00:24:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,5 @@
 katz_deli = [] 
 
-def take_a_number(array, name)
-  array.push(name)
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position 
-end 
-
 
 def line(array) 
   if array.length == 0 
