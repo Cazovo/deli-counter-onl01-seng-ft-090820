4c4d0c4861b2a50ec2b3fda9404bfce0d6513fc0
conner fortner <fortner.conner@yahoo.com>
2020-09-17 00:14:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,4 @@
-def line(deli)
-  if array.length >= 1
-    newarray = []
-    counter = 1 
-    deli.each do |name|
-      newarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{newarray.join(" ")}"
-  else
-    puts "The line is currently empty."
-  end
-end
+
 
 
 def take_a_number(line, new_customer)
