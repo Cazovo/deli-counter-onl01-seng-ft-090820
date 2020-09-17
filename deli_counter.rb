8f53447a4ba2c332870503a950c1ffcb01f956f9
conner fortner <fortner.conner@yahoo.com>
2020-09-17 00:24:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,18 +12,7 @@ def line(array)
   end 
 end 
 
-def line(array) 
-  if array.length == 0 
-    puts "The line is currently empty."
-  else 
-    message = "The line is currently:"
-    
-  array.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
-  end 
-  puts "#{message}"
-  end 
-end 
+
  
 
 
@@ -36,7 +25,7 @@ def now_serving(array)
   end 
 end 
 
-now_serving(katz_deli)
+
 
 
     
